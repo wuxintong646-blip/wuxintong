0d1e7174251574d48b94784d48885b6b9406553b
Wu xintong <2635813902@qq.com>
2026-03-13 23:22:19 +0800

    Simplify next.config.mjs for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: 'export', // 开启静态导出
+  images: { unoptimized: true }, // 静态模式必须关闭图片优化
+};
 
-export default nextConfig
+export default nextConfig;
